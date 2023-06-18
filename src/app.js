9ebb0fb67e4707e615e5407d0230012e2b463f59
fernando <isaacfernando5351@gmail.com>
2023-06-17 18:45:02 -0600

    adding middlewares, routes, cors configuration and accepting json and form/data

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
-const express =  require("express");
+const express =  require('express');
+const cors = require('cors');
 const port = process.env.PORT || 9000;
+const routerApi = require('./routes');
+const boom = require('@hapi/boom');
 
 //initialization
 const app = express();
 
 //config
-app.set("port", port);
+app.set('port', port);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//middlewares
-app.use ( function ( req , res , next ) {
-    res.header ( 'Access-Control-Allow-Origin' , "*" ) ;
-    res.header ( 'Access-Control-Allow-Methods' , 'GET, PUT, POST, DELETE');
-    res.header ( 'Access-Control-Allow-Headers' , "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+//middlewar
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error(boom.forbidden('Access denied due to CORS security restrictions')));
+    }
   }
-); 
+}
+app.use(cors(options));
 
 //routes
+routerApi(app);
+
+//middlewares
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 module.exports = app;
