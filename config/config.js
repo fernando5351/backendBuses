d234a0322b1da82ad52750e2767834d040a9b7e6
fernando <isaacfernando5351@gmail.com>
2023-06-17 18:27:25 -0600

    Configuration for Connections with Environment Variables

--- config/config.js
+++ config/config.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
+
 const config = {
     env: process.env.NODE_ENV || 'dev',
-    dbUser: process.env.PGUSER,
-    dbPassword: process.env.PGPASSWORD,
-    dbName: process.env.PGDATABASE,
-    dbHost: process.env.PGHOST,
-    dbPort: process.env.PGPORT
+    PORT: process.env.PORT,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+    dbHost: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT
 };
+
+// console.log(config.PORT + ' => port');
+// console.log(config.dbUser + ' => dbUser');
+// console.log(config.dbPassword + ' => dbPassword');
+// console.log(config.dbName + ' => dbName');
+// console.log(config.dbHost + ' => dbHost');
+// console.log(config.dbPort + ' => dbPort');
 
 module.exports = { config };
