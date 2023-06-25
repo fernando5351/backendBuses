24f5f196cc776bdadd85ab63732063276b7f8c1d
fernando <isaacfernando5351@gmail.com>
2023-06-25 07:54:29 -0600

    schema of user for validation

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -2,7 +2,7 @@ const joi = require('joi');
 
 const id = joi.number().integer();
 const username = joi.string().min(3).max(25);
-const email = joi.string().email({ tlds: { allow: ['com', 'net', 'yahoo'] } }).min(5).max(75);
+const email = joi.string().email().min(5).max(75);
 const password = joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(6).max(22);
 const roleId = joi.number().integer();
 
@@ -10,7 +10,7 @@ const createUser = joi.object({
 	username: username.required(),
 	email: email.required(),
 	password: password.required(),
-	roleId: roleId.required().validate( undefined, { presence: 'is required' })
+	roleId: roleId.required()
 });
 
 const getUser = joi.object({
