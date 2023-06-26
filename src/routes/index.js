00d821c75823a8fb13ca6aa85e2644f461c9823b
fernando <isaacfernando5351@gmail.com>
2023-06-25 22:35:22 -0600

    [:sparkles: add] auth route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const router =  require('express').Router();
 
 const roleRoute = require('./roleRoute');
 const usersRouter = require('./userRoute');
+const AuthRouter = require('./authRoute');
 
 function routerApi(app){
-	//http://localhost:300/api/v1/role
+	//http://localhost:300/api/v1/user/register
   app.use('/api/v1', router);
 	router.use('/role', roleRoute);
 	router.use('/user', usersRouter);
+	router.use('/auth', AuthRouter);
 }
 
 module.exports = routerApi;
