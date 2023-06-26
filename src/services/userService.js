e794aa0fe70e495d56cacf266057ad6414ba2abb
fernando <isaacfernando5351@gmail.com>
2023-06-26 01:22:47 -0600

    [:sparkles: add] send mail verification

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,33 +2,47 @@ const boom = require('@hapi/boom');
 const { models } = require('../../libs/sequelize');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
+const mailerService = require('./mailerService');
+
+const service = new mailerService;
 
 class UserServie {
+
+	code(){
+		const caracteres = '0123456789';
+		let codigo = '';
+
+		for (let i = 1; i < 6; i++) {
+			const indice = crypto.randomInt(1, caracteres.length);
+			codigo += caracteres[indice];
+		}
+		return codigo;
+	}
 
 	async create(data) {
 		try {
 			const pass = await bcrypt.hash(data.password, 10);
-			const tokenCode = crypto.randomBytes(5).toString('hex');
+			//const tokenCode = crypto.randomBytes(3).toString('hex');
 			const userData = {
 				...data,
 				password: pass,
-				verificationCode: tokenCode
+				verificationCode: this.code()
 			}
 			const user = await models.User.create(userData);
+			const rta = await service.sendMail(user, 'verification code', `<h1><strond>${this.code()}</strond></h1>`);
 			return {
 				status: 201,
 				message:"New User created",
 				user
 			};
 		} catch (error) {
-			console.log('Error in creating a new user')
 			throw error;
 		}
 	}
 
 	async getAll(){
 		let user =  await models.User.findAll();
-		if(!user || Array.isArray(user)){
+		if(!user){
 			throw boom.notFound("No records found");
 		} else {
 			return Promise.resolve({
@@ -39,25 +53,24 @@ class UserServie {
 		}
 	}
 
-	async getByPk(id) {
+	async getByPk(id, payload) {
 		const user = await models.User.findByPk(id);
 		if (!user) {
-			throw boom.notFound('User not found!');
+			throw boom.notFound("User not found!");
+		} if (payload.sub !== user.id) {
+			throw boom.unauthorized("you are not authorized");
 		}
 		return {
 			status: 302,
 			message: `User ${id} fetched`,
 			user
-		}
+		};
 	}
 
 	async getByEmail(email) {
 		const user = await models.User.findOne({
 			where:{ email }
 		});
-		if (!user) {
-			throw boom.notFound('User not found!');
-		}
 		return {
 			status: 302,
 			message: `User ${email} fetched`,
