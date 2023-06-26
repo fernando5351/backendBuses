cb611782d69cdbc80709b9688edde788f43b4449
fernando <isaacfernando5351@gmail.com>
2023-06-25 21:50:50 -0600

    [:bug: fix] role bug

--- database/migrations/20230617043932-Role.js
+++ database/migrations/20230617043932-Role.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { ROLE_TABLE, RoleSchema } = require('../models/RoleModel')
+const { ROLE_TABLE, RoleSchema } = require('../models/RoleModel');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ROLE_TABLE, RoleSchema);
+    await queryInterface.createTable(ROLE_TABLE,RoleSchema);
   },
 
   async down (queryInterface) {
