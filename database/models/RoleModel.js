6cfa8b047c76161485dded18643958453c60170d
fernando <isaacfernando5351@gmail.com>
2023-06-25 22:04:18 -0600

    [:sparkles: add] status

--- database/models/RoleModel.js
+++ database/models/RoleModel.js
@@ -1,39 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 
-const ROLE_TABLE = 'roles';
+const ROLE_TABLE = 'role';
 
 const RoleSchema = {
     id: {
-        allowNull: false,
         primaryKey: true,
         autoIncrement: true,
         type: DataTypes.INTEGER,
+        allowNull: false,
     },
     name: {
-        unique: true,
         allowNull: false,
-        type: DataTypes.STRING,
+				unique: true,
+        type: DataTypes.STRING
     },
-};
+    status: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+}
 
 
 class Role extends Model {
-    static associate(models) {
+    static associate(models){
         this.hasMany(models.User, {
             as: 'Users',
             foreignKey: 'roleId'
-        })
-    };
+        });
+    }
 
     static config(sequelize){
         return {
             sequelize,
-            modelName: 'Role',
             tableName: ROLE_TABLE,
+            modelName: 'Role',
             timestamps: false
-        };
-    };
-};
+        }
+    }
+}
 
 module.exports = {
     ROLE_TABLE,
