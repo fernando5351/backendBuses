a2d74ef3b4a755dcfec7ee17be9571f1cb371800
fernando <isaacfernando5351@gmail.com>
2023-06-27 00:51:09 -0600

    [:sparkles: add] admin insert register

--- database/migrations/20230617043932-Role.js
+++ database/migrations/20230617043932-Role.js
@@ -6,6 +6,9 @@ const { ROLE_TABLE, RoleSchema } = require('../models/RoleModel');
 module.exports = {
   async up (queryInterface) {
     await queryInterface.createTable(ROLE_TABLE, RoleSchema);
+		await queryInterface.bulkInsert(ROLE_TABLE, [{
+      name: 'admin',
+    }], {});
   },
 
   async down (queryInterface) {
