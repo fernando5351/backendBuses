8693d1192793e9f0d8acceb79d098d376d6c12d6
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-06-30 11:52:48 -0600

    [:sparkles: feat] add validation schema of stops

---
+++ src/schemas/stopSchema.js
@@ -0,0 +1,29 @@
+const joi = require('joi');
+
+const id = joi.number().integer();
+const stopName = joi.string().min(3).max(25);
+const lat = joi.number().precision(6);
+const long = joi.number().precision(6);
+
+const createStop = joi.object({
+	lat: lat.required(),
+	long: long.required(),
+	stopName,
+});
+
+const getStop = joi.object({
+	id: id.required(),
+});
+
+const updateStop = joi.object({
+	id,
+	lat,
+	long,
+	stopName,
+});
+
+module.exports = {
+	createStop,
+	getStop,
+	updateStop,
+};
